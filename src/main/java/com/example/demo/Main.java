6a7156dd47096b79025700a1b734beaab5950739
wx <996776029@qq.com>
2020-07-25 23:43:09 +0800

    commit observer design pattern

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+/*
+ * Claire
+ * @date  2020/7/25 下午9:55
+ *
+ */
+
+import observerdesignpattern.weatherstation.CurrentConditionsDisplay;
+import observerdesignpattern.weatherstation.WeatherData;
+
+
+public class Main {
+    public static void main(String[] args) {
+       /* ConcreteSubject concreteSubject=new ConcreteSubject();
+        concreteSubject.registerObserver(new ConcreteObserverOne());
+        concreteSubject.registerObserver(new ConcreteObserverTwo());
+        concreteSubject.notifyObserver(new Message(111,"wenxi"));*/
+        WeatherData weatherData=new WeatherData();
+        CurrentConditionsDisplay currentConditionsDisplay=new CurrentConditionsDisplay(weatherData);
+        weatherData.registerObserver(currentConditionsDisplay);
+        weatherData.setMeasurements(88,89,78);
+    }
+}
